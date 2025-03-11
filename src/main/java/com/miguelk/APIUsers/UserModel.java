4e7bf5933879eb6ad24611b9c1fee1bcb06de12f
Miguel Kapicius <miguelkapicius@gmail.com>
2025-03-11 20:16:04 -0300

    Cria uma entidade no banco de dados a partir do UserModel

--- src/main/java/com/miguelk/APIUsers/UserModel.java
+++ src/main/java/com/miguelk/APIUsers/UserModel.java
@@ -1,8 +1,48 @@
 package com.miguelk.APIUsers;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_register")
 public class UserModel {
-    private String id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private int age;
     private String email;
+
+    public UserModel() {
+    }
+
+    public UserModel(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
